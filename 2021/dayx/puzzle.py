90d59ae00c289fc30497fe74ecb58c385140a6fc
James Purcell <jamespur@vt.edu>
2021-12-06 18:38:27 -0500

    reorganize

---
+++ 2021/dayx/puzzle.py
@@ -0,0 +1,22 @@
+def parseInput():
+    with open("input.txt") as file:
+        # Parse input
+        None
+    
+    return None
+
+def part1():
+    inputData = parseInput()
+
+    print("Not yet implemented")
+
+def part2():
+    inputData = parseInput()
+
+    print("Not yet implemented")
+
+print("Part 1 solution:")
+part1()
+
+print("Part 2 solution:")
+part2()
