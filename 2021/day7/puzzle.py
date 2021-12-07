c360b92d97a8693d41613a18455f630ac1b2c6b1
James Purcell <jamespur@vt.edu>
2021-12-07 00:29:59 -0500

    day 7 compact

--- 2021/day7/puzzle.py
+++ 2021/day7/puzzle.py
@@ -1,32 +1,17 @@
-from aocd import get_data # @ignore
+from aocd import get_data
 
 def parseInput():
     data = get_data(day=7) # str
     data = [int(a) for a in data.split(",")]
-    
     return data
 
 def part1():
     data = parseInput()
-
-    fuels = [None] * (max(data))
-    for i in range(min(data), max(data)):
-        fuel = [abs(a-i) for a in data]
-        fuels[i] = sum(fuel)
-    
-    min_fuel = min([fuel for fuel in fuels if fuel is not None])
-    return min_fuel
+    return min([sum([abs(a-i) for a in data]) for i in range(min(data), max(data))])
 
 def part2():
     data = parseInput()
-
-    fuels = [None] * (max(data))
-    for i in range(min(data), max(data)):
-        fuel = [abs(a-i) * (abs(a-i) + 1) // 2 for a in data]
-        fuels[i] = sum(fuel)
-    
-    min_fuel = min([fuel for fuel in fuels if fuel is not None])
-    return min_fuel
+    return min([sum([abs(a-i) * (abs(a-i) + 1) // 2 for a in data]) for i in range(min(data), max(data))])
 
 
 print("Part 1 solution:")
