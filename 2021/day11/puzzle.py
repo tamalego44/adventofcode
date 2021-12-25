2baa3a150d578ae4eee2999b1a19724fb8b6a638
jamespur <jamespur@vt.edu>
2021-12-24 22:17:07 -0500

    day 11

--- 2021/day11/puzzle.py
+++ 2021/day11/puzzle.py
@@ -2,7 +2,7 @@
 def parseInput():
     data = []
 
-    if True:
+    if False:
         filename = "sample.txt"
     else:
         filename = "input.txt"
@@ -18,7 +18,7 @@ def parseInput():
 def flash(data, i0, j0):
     for i in range(max(0,i0-1), min(len(data), i0+2)): 
         for j in range(max(0,j0-1), min(len(data[0]), j0+2)):
-            if not (i == i0 and j == j0):
+            if not (i == i0 and j == j0) and data[i][j] != -1:
                 #print((i, j))
                 data[i][j] += 1
     return data
@@ -26,6 +26,7 @@ def flash(data, i0, j0):
 def part1():
     data = parseInput()
 
+    flashes = 0
     for step in range(100):
         done = False
         #Updating
@@ -33,23 +34,64 @@ def part1():
                 for j in range(len(data)):
                     data[i][j] += 1
         
-        flashes = 0
         #Flashing
         while not done:
             done = True
             for i in range(len(data)):
                 for j in range(len(data)):
-                    if data[i][j] >= 9:
+                    if data[i][j] > 9:
                         #print((i,j), "===")
                         flashes += 1
                         data[i][j] = -1
                         data = flash(data, i, j)
+                        done = False
                         #exit()
         
+        #restore 
+        for i in range(len(data)):
+            for j in range(len(data)):
+                if data[i][j] == -1:
+                    data[i][j] = 0
+        
     print(data)
+    return flashes
 
 def part2():
     data = parseInput()
+
+    step = 0
+    while True:
+        step += 1
+        done = False
+        #Updating
+        for i in range(len(data)):
+                for j in range(len(data)):
+                    data[i][j] += 1
+        
+        #Flashing
+        while not done:
+            done = True
+            for i in range(len(data)):
+                for j in range(len(data)):
+                    if data[i][j] > 9:
+                        #print((i,j), "===")
+                        data[i][j] = -1
+                        data = flash(data, i, j)
+                        done = False
+                        #exit()
+        
+        #restore 
+        simultaneous = True
+        for i in range(len(data)):
+            for j in range(len(data)):
+                if data[i][j] == -1:
+                    data[i][j] = 0
+                else:
+                    simultaneous = False
+        
+        if simultaneous:
+            return step
+        
 
 print("Part 1 solution:")
 print(part1())
